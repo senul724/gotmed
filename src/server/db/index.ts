42fc2d38284747d0f6b437a6bec2a1998ea2914f
senul ranasinghe <hardlove.mrplus@gmail.com>
2024-10-11 22:15:03 +0530

    initiated neon postgres db with drizzle

---
+++ src/server/db/index.ts
@@ -0,0 +1,17 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "@/env";
+import * as schema from "./schema";
+
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+	conn: ReturnType<typeof neon> | undefined;
+};
+
+const conn = globalForDb.conn ?? neon(env.DATABASE_URL);
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn, { schema });
---
+++ src/server/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTableCreator, varchar } from "drizzle-orm/pg-core";
+
+export const createTable = pgTableCreator((name) => `park_${name}`);
+
+export const users = createTable("users", {
+  id: varchar("id", { length: 40 }).primaryKey(),
+  name: varchar("name", { length: 200 }).notNull(),
+  email: varchar("email", { length: 100 }).notNull().unique(),
+  image: varchar("image", { length: 200 }),
+});
